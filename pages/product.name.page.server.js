3bf9413532c8136e537f96058d135193fd6edabf
rrd <rrd@1108.cc>
2022-05-27 11:47:08 +0200

    product page added

---
+++ pages/product.name.page.server.js
@@ -0,0 +1,21 @@
+export async function onBeforeRender(pageContext) {
+  // The route parameter of `/product/:name` is available at `pageContext.routeParams`
+  const { productName } = pageContext.routeParams
+
+  // Our render and hydrate functions we defined earlier pass `pageContext.pageProps` to
+  // the root Vue component `Page`; this is where we define `pageProps`.
+  const pageProps = { productName }
+
+  // We make `pageProps` available as `pageContext.pageProps`
+  return {
+    pageContext: {
+      pageProps,
+    },
+  }
+}
+
+// By default `pageContext.*` are available only on the server. But our hydrate function
+// we defined earlier runs in the browser and needs `pageContext.pageProps`; we use
+// `passToClient` to tell `vite-plugin-ssr` to serialize and make `pageContext.pageProps`
+// available to the browser.
+export const passToClient = ['pageProps', 'urlPathname']
---
+++ pages/product.name.page.vue
@@ -0,0 +1,10 @@
+<script>
+  const pageProps = ['productName']
+  export default { props: pageProps }
+</script>
+
+<template>
+  <h1>Termék: {{ productName }}</h1>
+</template>
+
+<style scoped></style>
