97b262d0759d978df090577c6970d24eb65c2f28
rrd <rrd@1108.cc>
2022-05-27 11:46:52 +0200

    client router set up

--- renderer/_default.page.client.js
+++ renderer/_default.page.client.js
@@ -1,12 +1,32 @@
-import { getPage } from 'vite-plugin-ssr/client'
 import { createApp } from './app'
+import { useClientRouter } from 'vite-plugin-ssr/client/router' // https://vite-plugin-ssr.com/useClientRouter
 
-hydrate()
+let app
+const { hydrationPromise } = useClientRouter({
+  render(pageContext) {
+    if (!app) {
+      app = createApp(pageContext)
+      app.mount('#app')
+    } else {
+      app.changePage(pageContext)
+    }
+  },
+  // Vue needs the first render to be a hydration
+  ensureHydration: true,
+  prefetchLinks: true,
+  onTransitionStart,
+  onTransitionEnd,
+})
 
-async function hydrate() {
-  // We do Server Routing, but we can also do Client Routing by using `useClientRouter()`
-  // instead of `getPage()`, see https://vite-plugin-ssr.com/useClientRouter
-  const pageContext = await getPage()
-  const app = createApp(pageContext)
-  app.mount('#app')
+hydrationPromise.then(() => {
+  console.log('Hydration finished; page is now interactive.')
+})
+
+function onTransitionStart() {
+  console.log('Page transition start')
+  document.querySelector('.content').classList.add('page-transition')
+}
+function onTransitionEnd() {
+  console.log('Page transition end')
+  document.querySelector('.content').classList.remove('page-transition')
 }
